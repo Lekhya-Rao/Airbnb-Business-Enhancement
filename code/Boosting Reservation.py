9809f7156999127085979e6b276c803a11275497
Lekhya-Rao <89992523+Lekhya-Rao@users.noreply.github.com>
2021-09-27 20:13:07 -0400

    Add files via upload

---
+++ code/Boosting Reservation.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import math
+from sklearn import linear_model
+import sklearn
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error, accuracy_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+def mape(actual, predicted):
+    return np.mean(np.abs((actual - predicted) / actual)) * 100
+# reading datasets
+london = pd.read_csv('London_Reservations_Final.csv')
+nyc = pd.read_csv("NewYork_Reservations_Final.csv")
+# preparing regression model
+reg = linear_model.LinearRegression()
+reg1 = linear_model.LinearRegression()
+# dividing datasets into training and test sets
+lx_train, lx_test, ly_train, ly_test = train_test_split(london[['no_of_amenities', 'price']], london.reservations,
+                                                        test_size=0.3, random_state=0)
+nx_train, nx_test, ny_train, ny_test = train_test_split(nyc[['no_of_amenities', 'price']], nyc.reservations,
+                                                        test_size=0.3, random_state=0)
+# building regression model with training set
+reg.fit(lx_train, ly_train)
+reg1.fit(nx_train, ny_train)
+# predicting values of test set
+l_pred = reg.predict(lx_test)
+n_pred = reg1.predict(nx_test)
+# computing r^2 score for regression model
+print('london', reg.score(lx_test, ly_test))
+print('nyc', reg.score(nx_test, ny_test))
+# computing MSE for regression model
+mse = sklearn.metrics.mean_squared_error(ly_test, l_pred)
+math.sqrt(mse)
+print("mse: ", math.sqrt(mse))
+result = mape(ly_test, l_pred)
+result1 = mape(ny_test, n_pred)
+print("MAPE: London ", result)
+print("MAPE: New York ", result1)
---
+++ code/KPrototype Clustering.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from kmodes.kprototypes import KPrototypes as kp
+df = pd.read_csv('/content/Listings_Review_(join)NewYork.csv')
+df['neighbourhood_cleansed'] = df['neighbourhood_cleansed'].fillna('NA')
+df['total_listings'] = df['total_listings'].fillna('0')
+df['total_reviews'] = df['total_reviews'].fillna('0')
+df['minimum_price'] = df['minimum_price'].fillna('0')
+df['maximum_price'] = df['maximum_price'].fillna('0')
+df['avg_price'] = df['avg_price'].fillna('0')
+df['avg_review_score'] = df['avg_review_score'].fillna('0')
+df['min_review_score'] = df['min_review_score'].fillna('0')
+df['max_review_score'] = df['max_review_score'].fillna('0')
+df.head()
+# KPrtotypes requires values to be used as array
+df_array = df.values
+# specify numerical attributes
+for i in range(1, 9):
+    df_array[:, i] = df_array[:, i].astype(float)
+    reviews_per_listing = []
+# dividing dataset into clusters
+kproto = kp(n_clusters=3, verbose=2)
+clusters = kproto.fit_predict(df_array, categorical=[0])
+# adding cluster as a column to dataset
+df['cluster'] = clusters
+df.to_excel('neighbhorhood cluster NYC.xlsx')
+# creating excel sheets cluster-wise
+neighbhorhood_cluster0 = df[df['cluster'] == 0]
+neighbhorhood_cluster0.head()
+neighbhorhood_cluster0.to_excel('neighbhorhood_NYC_cluster0.xlsx')
+neighbhorhood_cluster1 = df[df['cluster'] == 1]
+neighbhorhood_cluster1.head()
+neighbhorhood_cluster1.to_excel('neighbhorhood_NYC_cluster1.xlsx')
+neighbhorhood_cluster1 = df[df['cluster'] == 2]
+neighbhorhood_cluster1.head()
+neighbhorhood_cluster1.to_excel('neighbhorhood_NYC_cluster2.xlsx')
